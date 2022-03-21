85b212487486a9d2d1ed424bd68d1906d5d7b462
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-03-21 12:55:14 +0530

    pr

---
+++ script.js
@@ -0,0 +1,13 @@
+let arr = [1,2,4,5,6,7];
+let arr2 = [6,7,8,9,0];
+console.log(arr); 
+console.log(arr.concat(arr2))
+//copyWithin(Target,start,end)
+console.log(arr.copyWithin(0,2,4))
+// arr = arr2;
+let arr1 = [1,2,4,5,6,7];
+const f = arr1.entries();
+console.log(f)
+for(let x of f){
+    console.log(x)
+}
