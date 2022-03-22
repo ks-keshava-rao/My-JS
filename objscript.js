37d6db3c6714417b46892a444adc217ea81c605b
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-03-22 18:15:24 +0530

    ps

--- objscript.js
+++ objscript.js
@@ -7,19 +7,38 @@ let obj = {
                console.log(this.firstName + " " + this.lastName);
     }
 }
+//object utility methods
+console.log( )
 obj.display()
 
-// object constructor
-let  obj2 = new Object();
-obj.firstName = "rahul";
-console.log(obj2)
-
 //factory function 
- function obj2 (){
+function createdata(firstName,lastName)
+{
     return {
-         age : 33
-     }
- }
- console.log(obj2)
+        firstName : firstName,
+        lastName : lastName,
+        getfullname(){
+            console.log(this.firstName + " " + this.lastName);
+        }
+    }
+}
 
-//Object create 
+let person1 = new createdata('keshav','rao');
+let person2 = new createdata('robert','drowney');
+person1.getfullname()
+person2.getfullname()
+
+function cardata(name,model,year){
+ this.name = name;
+ this.model = model;
+ this.year = year;
+ this.display = function(){
+     console.log(this.name + " "+this.model+" "+this.year);
+ };
+}
+let car1 = new cardata('huyndai','suv',2022);
+let car2 = new cardata('skoda','cruiser',2021);
+car1.display()
+car2.display()
+console.log(window.display())
+//iteratables map set , weak map and weak set, iimedietly invoking functions , call ,binding,apply,
