c3e8d150a068be43b55946bba742d0555777be90
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-03-22 18:51:05 +0530

    ps

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const outputo = users.filter(x => x.age<40)
 console.log(outputo); 
 //using reduce
 const outputx = users.reduce(function(acc,curr){
-    if(curr.age<=30){
+    if(curr.age<=40){
         acc.push(curr.firstName);
     }
     return acc;
