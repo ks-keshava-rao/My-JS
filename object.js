58f2152aec0751a2d1eb68522cb5becb8a376027
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-03-24 17:11:31 +0530

    PR

--- object.js
+++ object.js
@@ -1,9 +1,12 @@
 let input = 'name';
 let my = {
-    title :'loops',
+    title :'bollywood',
     videolength :15 ,
     name:"kkr",
-    description : " this it this"
+    description : " this it this",
+    creator : "arijits singh",
+    copyrights : "Eros now",
+    lyrics : "javed ali"
 }
 //console.log(my)
 console.log(my.name)
@@ -13,4 +16,19 @@ console.log(my['name'])
 my['name'] = "cringe"
 console.log(my['name'])
 console.log(my[input])
-
+// iterable objcets
+let keys  = Object.keys(my);
+console.log(keys)
+let vals =  Object.values(my);
+console.log(vals)
+let entries =  Object.entries(my)
+console.log(entries)
+// if('name' in keys) 
+// console.log( 'find' )
+// else 
+//find key in object
+let ut = 'title' in my;
+let out4 = 'kk' in my;
+console.log(out4);
+out2 = my.hasOwnProperty('w')
+console.log(out2);
