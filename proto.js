0409452d637a75659841508dd664b25f690e3f8a
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-04 20:54:10 +0530

    pr

--- proto.js
+++ proto.js
@@ -1,1 +1,14 @@
-   
+function  create(firstname , lastname){
+    this.firstname = firstname;
+    this.lastname = lastname ;
+}
+let names2 = new create("keshav","rao");
+console.log(names2);
+let obj = {
+    firstname : "keshva",
+    lastname : "rao"
+}
+create.prototype.getname = function(){
+    return this.firstname+" "+this.lastname;
+}
+console.log(names2.getname())
