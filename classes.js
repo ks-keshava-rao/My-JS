854c446a68983ea1724e64c3039f1df36cfa93dd
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-06 01:08:10 +0530

    pr

--- classes.js
+++ classes.js
@@ -5,11 +5,26 @@ class  car {
         this.engine = engine;
         this.yom = yom;
     }
+    // let year = 2022;
+
     //class methods 
     carInfo(){
         return `This is manufactured by ${this.company} model ${this.model} with ${this.engine} engine in the year ${this.yom}`
     }
+    runningfrom(){
+        return 2022 - this.yom ;
+    }
+    static add(a,b)
+    {
+        return a+b; 
+    }
 }
-let nano = new car("tata","x2","bs8",2030);
+let nano = new car("tata","x2","bs8",2014);
 console.log(nano)
 console.log(nano.carInfo())
+console.log(nano.runningfrom())
+console.log(car.add(34,55))
+//inheritance 
+class sportsCar extends car {
+    constructor()
+}
