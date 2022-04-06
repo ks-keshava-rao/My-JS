0667db9f4341e87bffaa64ec676f162e1b452971
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-06 17:32:48 +0530

    pr

--- classes.js
+++ classes.js
@@ -1,3 +1,4 @@
+// classes do not support hoisting
 class  car {
     constructor(company,model,engine,yom){
         this.company = company;
@@ -12,22 +13,48 @@ class  car {
         return `This is manufactured by ${this.company} model ${this.model} with ${this.engine} engine in the year ${this.yom}`
     }
     runningfrom(){
-        return 2022 - this.yom ;
+        return `Running from ${2022 - this.yom} years ` ;
     }
-    static add(a,b)
+    static currMileage(pre,curr)
     {
-        return a+b; 
+        return Math.abs(curr-pre); 
     }
 }
 let nano = new car("tata","x2","bs8",2014);
 console.log(nano)
 console.log(nano.carInfo())
 console.log(nano.runningfrom())
-console.log(car.add(34,55))
+console.log(car.currMileage(77,33))
 //inheritance 
 class sportsCar extends car {
     constructor(company,model,engine,yom,topSpeed,horsepower){
-        //to call the constructor of the parent class
         super(company,model,engine,yom)
-    }
+        this.topSpeed = topSpeed;
+        this.horsepower = horsepower;//to call the constructor of the parent class
+        
+    }        
+    category(){
+        if(this.topSpeed<200 &&( this.horsepower<50 )){
+            return "class A sports car";
+        }
+        else if(this.topSpeed<300 && (this.horsepower<100 )){
+            return "class B sports car";
+        }
+        else if(this.topSpeed<400 && (this.horsepower<200 )){
+            return "class C sports car";
+        }
+        else {
+            return 'Normal car'
+        }
+     }
+     static totalPower(ts,hs){
+        return ts*hs + " power";
+     }
+
 }
+
+let lambo = new sportsCar("lambo","mclaren","v12",2015,321,112);
+console.log(lambo.carInfo());
+console.log(lambo.runningfrom());
+console.log(lambo.category()); 
+console.log(sportsCar.totalPower(100,45)); 
