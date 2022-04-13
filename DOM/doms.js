325a5d41eef31d1013f14f7e68e2f1738e3b9a32
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-13 14:50:08 +0530

    pr

--- DOM/doms.js
+++ DOM/doms.js
@@ -1,5 +1,18 @@
 
+// for(let i=0 ; i<document.body.childNodes.length; i++){
+//     alert(document.body.childNodes[i]);
+// }
 for(let i=0 ; i<document.body.childNodes.length; i++){
-    alert(document.body.childNodes[i]);
+    console.log(document.body.childNodes[i]);
 }
-alert(document.body.childElementCount)
+console.log(document.body.childElementCount);
+console.log(document.body.childNodes[0]===document.body.firstChild);
+console.log(document.body.childNodes[document.body.childNodes.length - 1]===document.body.lastChild);
+
+for(let node of document.body.childNodes){
+    console.log(node);
+}
+
+console.log(document.body.parentNode === document.documentElement);
+console.log(document.head.nextSibling);
+console.log( document.body.previousSibling ); // HTMLHeadElement
