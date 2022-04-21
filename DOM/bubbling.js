1ea8af1feb091a37e195b9cfc853f11081b3a287
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-21 19:23:56 +0530

    ps

---
+++ DOM/bubbling.js
@@ -0,0 +1,12 @@
+document.querySelector('#grandparent')
+.addEventListener('click',()=>{
+    console.log("grandparent clicked");
+})
+document.querySelector('#parent')
+.addEventListener('click',()=>{
+    console.log("parent clicked");
+})
+document.querySelector('#child')
+.addEventListener('click',()=>{
+    console.log("child clicked");
+})
