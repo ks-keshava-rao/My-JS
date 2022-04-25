fa39bae0b502bb546843f680a358e7ffb1b115dd
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-25 12:11:16 +0530

    pr

--- DOM/bubbling.js
+++ DOM/bubbling.js
@@ -1,12 +1,29 @@
+//EVENT CAPTURING
 document.querySelector('#grandparent')
 .addEventListener('click',()=>{
     console.log("grandparent clicked");
-})
+},false)
 document.querySelector('#parent')
-.addEventListener('click',()=>{
+.addEventListener('click',(e)=>{
     console.log("parent clicked");
-})
+    e.stopPropagation();
+},false)
 document.querySelector('#child')
 .addEventListener('click',()=>{
     console.log("child clicked");
-})
+},false)
+//by default if the false argument is not passed then
+ //it is in bubbling mode
+ //EVENT BUBBLING
+// document.querySelector('#grandparent')
+// .addEventListener('click',()=>{
+//     console.log("grandparent clicked");
+// },false)
+// document.querySelector('#parent')
+// .addEventListener('click',()=>{
+//     console.log("parent clicked");
+// },false)
+// document.querySelector('#child')
+// .addEventListener('click',()=>{
+//     console.log("child clicked");
+// },false)
