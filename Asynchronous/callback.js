be8a1e358f0b26f3060d10fea3a189c7c63ff627
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-04-30 14:45:43 +0530

    pr

--- Asynchronous/callback.js
+++ Asynchronous/callback.js
@@ -12,21 +12,27 @@ const Techdet = [
 function register(details) {
     setTimeout(function () {
         Techdet.push(details)
+        alert("Details updated succesfully")
     }, 3000);
 }
 function getdata() {
-    setTimeout(function () {
+    setTimeout(function () {                                                                                                                                                                                                                                                                                              
         let str = "";
-        Techdet.forEach(function(info) {
-            // str += `<li> ${info.company} </li>`
-            console.log(info)
+        Techdet.forEach(function(info) {                                                               
+            str += `<li class="list-group-item "> ${info.company} - ${info.product}  </li>`
+            alert("Details fetched")
         });
         document.getElementById('uldiv').innerHTML = str;
-    }, );
+    },1000 );
 }
 let newtech = {
     company: "microsoft",
     product: "Azure"
 }
+let newtech2 = {
+    company: "adobe",
+    product: "adobexd"
+}
 register(newtech);
+register(newtech2);
 getdata();
