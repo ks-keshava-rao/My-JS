c930aecc99fb0c05ab4bdb877eff612818796c35
k s keshava rao <66875322+kskrao219@users.noreply.github.com>
2022-05-06 19:09:09 +0530

    pr

--- Asynchronous/AsyncAwait.js
+++ Asynchronous/AsyncAwait.js
@@ -7,9 +7,24 @@ async function asynch(){
     console.log("users resolved");
     return users;
 }
-console.log("before calling");
-a = asynch()
+// console.log("before calling");
+// a = asynch()
+// console.log("after calling");
+// console.log(a);
+// a.then(data => console.log(data));
+// console.log("last line")
+
+async function calling(){
+a = await asynch()
 console.log("after calling");
 console.log(a);
 a.then(data => console.log(data));
 console.log("last line")
+}
+
+calling()
+
+//user management tool 
+//fetch and display 
+//edit button during display 
+//display the edited value 
